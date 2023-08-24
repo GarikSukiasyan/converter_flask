96a7d52e3713c66e74dd771372292ae9f1b6343c
User <garigin2002@gmail.com>
2023-08-24 20:37:00 +0300

    Небольшие правки в шаблонах, добавил output.html для редиректа на него и получения ссылки на скачивание

--- app/views/views.py
+++ app/views/views.py
@@ -3,19 +3,29 @@ from flask import render_template
 
 
 
+
 @app.route('/')
 def home():
     # Возвращаем шаблон страницы и передаем данные из базы
     return render_template("home.html")
 
+
 @app.route('/videos')
 def videos():
     return render_template("videos.html")
+
 
 @app.route('/documents')
 def documents():
     return render_template("documents.html")
 
+
 @app.route('/images')
 def images():
     return render_template("images.html")
+
+
+@app.route('/output/<path:id>')
+def output(id):
+    # Ваш код обработки перехода по URL с переменной id
+    return 'Переход по URL: output/{}'.format(id)
