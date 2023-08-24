5b2b68ac2ccdb60d2fb22e5593fa8090be1d74e8
Lind-it <poilpo227@gmail.com>
2023-08-24 22:01:57 +0300

    в app/__init__ подклбчены views

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,6 @@ app = Flask(__name__)
 # Импорт обработчика
 from .views import views
 from .views import views_other
+from .views import views_documents
+from .views import views_videos
+from .views import views_images
--- app/views/views_documents.py
+++ app/views/views_documents.py
@@ -2,7 +2,6 @@ from app import app
 from flask import render_template
 
 
-
 @app.route('/documents')
 def documents():
     return render_template("documents.html")
