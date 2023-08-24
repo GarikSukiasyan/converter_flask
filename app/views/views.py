04abe04bf805c92b23ae7fdb937bae56a568d3dd
Lind-it <poilpo227@gmail.com>
2023-08-24 17:33:30 +0300

    aded structure

--- app/views/views.py
+++ app/views/views.py
@@ -7,3 +7,15 @@ from flask import render_template
 def home():
     # Возвращаем шаблон страницы и передаем данные из базы
     return render_template("home.html")
+
+@app.route('/videos')
+def videos():
+    return render_template("videos.html")
+
+@app.route('/documents')
+def documents():
+    return render_template("documents.html")
+
+@app.route('/images')
+def images():
+    return render_template("images.html")
