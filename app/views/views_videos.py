b54780a46a9caf1fa34ef3bbf0aca215df852eb1
User <garigin2002@gmail.com>
2023-08-25 22:35:59 +0300

    Добавил папку output куда будут помещаться конверченные файлы, добавил работу с cookie в views_videos.py и добавил ffmpeg код. Нужно сделать progress бар и обработку на сороне сервера.

--- app/views/views_videos.py
+++ app/views/views_videos.py
@@ -1,7 +1,9 @@
 import os
 import random
+import subprocess
+
 from app import app
-from flask import request, flash, render_template, redirect, url_for
+from flask import request, flash, render_template, redirect, url_for, make_response
 from werkzeug.utils import secure_filename
 
 
@@ -27,7 +29,7 @@ def upload_file():
     if request.method == 'POST':
 
         format_video = request.form.get('my_select')
-        print(format_video)
+        # print(format_video)
 
         if 'file' not in request.files:
             flash('Не могу прочитать файл')
@@ -37,14 +39,32 @@ def upload_file():
             flash('Нет выбранного файла')
             return redirect(request.url)
         if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            print(filename)
+            response = make_response(redirect('/output/iasd231sd/'))
+
+            oldFileName = secure_filename(file.filename)
+            # print(filename)
 
             len_files = len(os.listdir("app/static/uploadFile/"))
             filename = int(len_files) + int(1)
             filename = str(file.filename) + "_" + str(filename) + str(".mp4")
 
+            # print(filename)
+
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect('/output/iasd231sd/')
+
+            response.set_cookie("id", "iasd231sd", max_age=60*60*24*365*2)
+            response.set_cookie("progressBar", "0", max_age=60 * 60 * 24 * 365 * 2)
+            response.set_cookie("inputFileName", oldFileName, max_age=60 * 60 * 24 * 365 * 2)
+            response.set_cookie("outputFileName", filename, max_age=60 * 60 * 24 * 365 * 2)
+
+            # https://pypi.org/project/ffmpeg-progress-yield/
+            # ffmpeg -i input.mkv -vcodec copy -acodec copy output.mov
+            subprocess.call([
+                "ffmpeg",
+                "-i", f"app/static/uploadFile/{filename}",
+                "-c:a", "copy",
+                f"app/static/output/{filename}.avi"])
+
+            return response
 
     return render_template("videos.html")
