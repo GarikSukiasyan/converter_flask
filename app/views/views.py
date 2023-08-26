64a6fc939ce22275c775c361efa69c383e9f2ae6
User <garigin2002@gmail.com>
2023-08-26 13:28:02 +0300

    Добавил пример работы flask_socketio, нужно заменить старые версии javascript на новые и решить вопрос с появляющейся рекламой

--- app/views/views.py
+++ app/views/views.py
@@ -1,7 +1,10 @@
+import threading
+import time
+
 from app import app
 from flask import render_template
 
-
+from app import app, socketio
 
 
 @app.route('/')
@@ -10,4 +13,23 @@ def home():
     return render_template("home.html")
 
 
+# Функция для выполнения подсчета
+def count_to_million():
+    for i in range(10):
+        time.sleep(1.0)
+        # Отправляем текущее значение на фронтенд
+        socketio.emit('count_update', {'count': i}, namespace='/test')
+
+
+# Обработчик для обработки подключения к сокету
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    print('Client connected')
+    # Запускаем фоновый поток для выполнения подсчета
+    threading.Thread(target=count_to_million).start()
+
+# Отправляем статический HTML-файл для отображения подсчета
+@app.route('/test')
+def test():
+    return render_template('test.html')
 
