54fea2418170352ab0d4d5bfe060c152dbba7eba
User <garigin2002@gmail.com>
2023-08-29 23:21:19 +0300

    Осталось сделать работу socketIo и первичную обработку json ответа на стороне html javascript

--- app/views/views_output.py
+++ app/views/views_output.py
@@ -16,24 +16,21 @@ list_file = []
 
 
 
-@app.route('/output/<path:id>')
-def output(id):
-    # print(all_user_key)
-    # Ваш код обработки перехода по URL с переменной id
-    return render_template("output.html")
-
 
 class ffm():
     def __init__(self):
         self.progress = 0
         self.status = False
+        self.file_id = ''
+        self.fileName = ''
 
-    def procc_ffmpeg(self, path, file_id):
-        # os.mkdir(f"app/static/output/{file_id}")
+    def procc_ffmpeg(self, file_id):
+
+        file_list = os.listdir(f"app/static/output/{file_id}/")
 
         cmd = [
             "ffmpeg",
-            "-i", f"app/static/uploadFile/{path}",
+            "-i", f"app/static/output/{file_id}/{file_list[0]}",
             "-c:a", "copy",
             f"app/static/output/{file_id}/output.mp4",
         ]
@@ -46,76 +43,77 @@ class ffm():
         list_file.remove(file_id)
         self.status = True
 
-    def start_thr(self, path, file_id):
+    def start_thr(self, file_id):
         # print('1')
-        Thread(target=self.procc_ffmpeg, args=(path, file_id, )).start()
+        Thread(target=self.procc_ffmpeg, args=(file_id, )).start()
 
     def info_thr(self, stat=None):
         if stat:
             self.progress = stat
         else:
-            return self.progress, self.status
+            return self.progress, self.status, self.file_id, self.fileName
 
 
 
 xt = ffm()
 
-@app.route("/ffmpeg_start", methods=["GET"])
-def ffmpeg_start():
-    file_id = request.cookies.get('id')
-    path = request.cookies.get('outputFileName')
-    # print(file_id)
-
-    if file_id:
-        if file_id not in list_file and file_id != "0":
-
-            if os.path.isdir(f"app/static/output/{file_id}"):
-                # Папка существует
-                data = {'progress': 100, 'download': True}
-
-                return jsonify(data)
-            else:
-                # Папка не существует
-
-                os.mkdir(f"app/static/output/{file_id}")
-
-                list_file.append(file_id)
-                # print('start ', file_id)
-
-                xt.procc_ffmpeg(path, file_id)
-                data = {'progress': 0, 'download': False}
-
-                return jsonify(data)
-
-        else:
-            progress, stat = xt.info_thr()
-            if stat == False:
-                data = {'progress': progress, 'download': False}
-                return jsonify(data)
-            elif stat == True:
-                # Передаем что бы создал кнопку для скачивания файла и больше не присылал запросы
-                data = {'progress': 100, 'download': True}
-
-                response = make_response(jsonify(data))
-                response.set_cookie("id", "0", max_age=60 * 60 * 24 * 365 * 2)
-                return response
-
-                # return jsonify(data)
 
 
-    stat = xt.info_thr()
-    data = {'progress': stat, 'download': True}
-    return jsonify(data)
+
+
+@app.route('/output/<path:file_id>')
+def output(file_id):
+    if os.path.isdir(f"app/static/output/{file_id}/"):
+        # print("Папка существует.")
+
+        # Проверяем наличие файлов
+        file_list = os.listdir(f"app/static/output/{file_id}/")
+        if len(file_list) == 2:
+            json_data = {'progress': 100, 'download': True, 'file_id': file_id, 'nameFile': file_list[1]}
+            return render_template("output.html", json_data=json_data)
+
+        elif len(file_list) == 1:  # Если один файл
+
+            list_file.append(file_id)
+
+            # Запускаем конверт
+            xt.start_thr(file_id)
+
+            json_data = {'progress': 0, 'download': False, 'file_id': file_id, 'nameFile': 'None'}
+            return render_template("output.html", json_data=json_data)
+
+    else:
+        # print("Папка не существует.")
+        return render_template('other/404.html'), 404
+
 
 
 @app.route("/ffmpeg_status", methods=["GET"])
 def ffmpeg_status():
-    data = {'all_num_mb': 0, 'all_num_file': 0}
-    return jsonify(data)
+    progress, stat, file_id, fileName = xt.info_thr()
+    if stat == False:
+        data = {'progress': progress, 'download': False, 'file_id': file_id, 'nameFile': fileName}
+        return jsonify(data)
+    elif stat == True:
+        # print("Папка существует.")
+        file_list = os.listdir(f"app/static/output/{file_id}/")
+
+        # Если два файла
+        if len(file_list) == 2:
+            if file_list[0].replace(".", " ") == "output":
+                # Возвращаем страницу + json progressbar: 100 status: True NameFile: file_list[0]
+                data = {'progress': 100, 'download': True, 'file_id': file_id, 'nameFile': fileName}
+                return jsonify(data)
+            elif file_list[1].replace(".", " ") == "output":
+                # Возвращаем страницу + json progressbar: 100 status: True NameFile: file_list[1]
+                data = {'progress': 100, 'download': True, 'file_id': file_id, 'nameFile': fileName}
+                return jsonify(data)
+            else:
+                data = {'progress': 0, 'download': False, 'file_id': 'None', 'nameFile': 'None'}
+                return jsonify(data)
 
 
 
-
-@app.route('/output/<path:filename>', methods=['GET', 'POST'])
-def download(filename):
-    return send_from_directory(directory='pdf', filename=filename)
+    # Ваш код обработки перехода по URL с переменной id
+    # json_data = {'file_id': file_id}
+    # return render_template("output.html", json_data=json_data)
