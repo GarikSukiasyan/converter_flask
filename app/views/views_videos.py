3a73cf4ccd8ec9f1f744340b73c21f4e6aae03d9
User <garigin2002@gmail.com>
2023-08-29 23:14:23 +0300

    Сделал, остается доделать output и socketIo

--- app/views/views_videos.py
+++ app/views/views_videos.py
@@ -1,9 +1,8 @@
 import os
 import random
 from app import app, all_user_key
-from flask import flash, render_template, redirect, make_response
-from werkzeug.utils import secure_filename
-from flask import jsonify, request
+from flask import flash, render_template, redirect
+from flask import request
 
 # папка для сохранения загруженных файлов
 UPLOAD_FOLDER = 'app/static/uploadFile/'
@@ -25,10 +24,6 @@ def allowed_file(filename):
 @app.route('/videos', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
-
-        format_video = request.form.get('my_select')
-        # print(format_video)
-
         if 'file' not in request.files:
             flash('Не могу прочитать файл')
             return redirect(request.url)
@@ -41,28 +36,15 @@ def upload_file():
             file_id = ''
             for i in range(24):
                 file_id += random.choice(chars)
-            response = make_response(redirect(f'/output/{file_id}'))
             all_user_key.append(file_id)
 
-            oldFileName = secure_filename(file.filename)
-            # print(filename)
+            os.mkdir(f"app/static/output/{file_id}")
 
-            len_files = len(os.listdir("app/static/uploadFile/"))
-            filename = int(len_files) + int(1)
-            filename = str(file.filename) + "_" + str(filename) + str(".mp4")
-            # print(filename)
+            file_name = file.filename
+            file_name = file_name.split(".")
 
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file.save(os.path.join(f"app/static/output/{file_id}", f"input.{file_name[1]}"))
 
-            response.set_cookie("id", file_id, max_age=60*60*24*365*2)
-            response.set_cookie("progressBar", "0", max_age=60 * 60 * 24 * 365 * 2)
-            response.set_cookie("inputFileName", oldFileName, max_age=60 * 60 * 24 * 365 * 2)
-            response.set_cookie("outputFileName", filename, max_age=60 * 60 * 24 * 365 * 2)
+            return redirect(f'/output/{file_id}')
 
-            # start_thr(filename, file_id)
-
-            return response
-
-    response = make_response(render_template("videos.html"))
-    response.set_cookie("id", "0", max_age=60 * 60 * 24 * 365 * 2)
-    return response
+    return render_template("videos.html")
